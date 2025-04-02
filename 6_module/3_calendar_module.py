771ac304651ac8e6b521b6a204ae603b272ca3fe
shubham-mhatre <mhatreshubham03@gmail.com>
2025-04-02 14:20:51 +0530

    calendar module python

---
+++ 6_module/3_calendar_module.py
@@ -0,0 +1,13 @@
+import calendar
+
+print("print all function about calendar")
+print(dir(calendar))
+
+print("print calendar of month of jan of 2025")
+print(calendar.month(2025,1))
+
+print("check if year is leap year for 2024")
+print(calendar.isleap(2024))
+
+print("check if year is leap year for 2025")
+print(calendar.isleap(2025))
