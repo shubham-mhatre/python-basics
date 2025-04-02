3f2aee4633e5fe9cf90dc0c959f2b2df6a3fac70
shubham-mhatre <mhatreshubham03@gmail.com>
2025-04-02 14:16:15 +0530

    math module python

---
+++ 6_module/2_math_module.py
@@ -0,0 +1,13 @@
+import math
+
+print("to know more about module use : dir function : dir(math) : ")
+print(dir(math))
+
+print("find square root of : 16 : math.sqrt(16)")
+print(math.sqrt(16))
+
+print("find floor root of : 2.3 : math.floor(2.3) ")
+print(math.floor(2.3))
+
+print("find ceil root of : 2.3 : math.ceil(2.3) ")
+print(math.ceil(2.3))
