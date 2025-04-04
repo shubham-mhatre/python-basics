1c9c3f4c8139a4f703dca004f83d2751dec25980
shubham-mhatre <mhatreshubham03@gmail.com>
2025-04-04 08:20:39 +0530

    json in python

--- 8_json/1_json.py
+++ 8_json/1_json.py
@@ -11,11 +11,15 @@ book["bob"]={
     "contact":988234678
 }
 
+print('type of object before using json  ',type(book))
+
 import json
 
 #dumps serializes a Python object into a JSON formatted string
 s=json.dumps(book)
-print(s)
+print('type of object after using json.dumps()  ',type(s))
+print('printing output of json.dumps ', s)
+
 
 #write to file
 with open("D:\\projects\\poc\\github\\shubham-mhatre\\python\\python-demo\\8_json\\book.txt","w") as f:
@@ -24,8 +28,20 @@ with open("D:\\projects\\poc\\github\\shubham-mhatre\\python\\python-demo\\8_jso
 #read from same file
 r=open("D:\\projects\\poc\\github\\shubham-mhatre\\python\\python-demo\\8_json\\book.txt","r")
 s=r.read()
-print(s)
+print('type of object after reading file ',type(s))
+print("printing output of r.read() ",s)
 
 #used to deserialize data from a file or stream, converting it back into Python objects
 j=json.loads(s)
-print(j)
+print('type of object after json.loads() ',type(j))
+print("printing output of json.loads() ", j)
+
+#print specific value like phone number of bob
+print("print specific value like phone number of bob")
+print(book["bob"]["contact"])
+
+
+#print all person : iterate over json array and print all json object
+print("print all person : iterate over json array and print all json object")
+for person in book:
+    print(book[person])
